cd8268e26610a77f8cd68e88a136eed7a9adf71a
Urko <regueiro.u@protonmail.com>
2024-02-21 19:05:45 +0100

    Final version

--- acercade.py
+++ acercade.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import cv2
 
 def info():
     st.markdown("""
@@ -6,11 +7,16 @@ def info():
                 .big-font {
                     font-size:20px;
                     color: #FAFAFA;
+                }
+                    
+                .patata {
+                    font-size:0.8px;
+                    color: #FAFAFA;
 
                 }
                 .sub-font {
                     font-size:22px;
-                    color: #FAA227;
+                    color: #FAA227; 
                     font-weight: bold;                    
 
                 }
@@ -53,6 +59,12 @@ def info():
             	.stTabs [aria-selected="true"] {
               		background-color: #111111;
             	}
+            	
+            	.st-emotion-cache-1ol4dec {
+            	background-color: #705202;
+            	}
+            	
+            	
 
             </style>""", unsafe_allow_html=True)
 
@@ -61,10 +73,83 @@ def info():
     ########################################### ACERCA DE #############################################
 
     st.markdown("<h2 style='text-align: center; font-size: 60px;color: orange; '>Información</h2>", unsafe_allow_html=True)
+    st.image("divider.png")
+    st.markdown(
+        '<p class="big-font">En esta sección, te damos la bienvenida a conocer al equipo que ha hecho posible este proyecto. Aquí encontrarás enlaces a nuestras páginas profesionales, donde puedes obtener más información sobre nosotros o ponerte en contacto.<br>Además, proporcionamos un acceso directo al repositorio del proyecto en cualquiera de los perfiles de github. Allí podrás explorar el código fuente y comprender en detalle el proceso ETL que hemos implementado, así como el EDA, la modelización y diseño de la página web, apreciando el trabajo y la dedicación que hemos invertido.<br>Esperamos que esta sección te brinde una comprensión más completa de quiénes somos y qué nos impulsa a seguir avanzando como científicos de datos. Te invitamos a explorar y descubrir más sobre nosotros y nuestros proyectos.</p>',
+        unsafe_allow_html=True)
 
+    ###################################################### INFO ######################################################
+    width, height = (255, 255)
+    col, columna_adrian, columna_eduardo, columna_esteban, columna_urko = st.columns((0.15,1,1,1,1))
 
+    ################################################################
+    ########## Adrián ##########
 
+    with columna_adrian:
+        st.header(":orange[&nbsp;&nbsp;&nbsp;&nbsp;_Adrián Moldes_]")
+        adrian = cv2.imread(r"data/Adrian.png")
+        adrian = cv2.cvtColor(adrian, cv2.COLOR_BGR2RGB)
+        adrian = cv2.resize(adrian, (width, height))
+        st.image(adrian)
 
+        # Links:
+        linkedin, github = st.columns((1.2,1))
+
+        linkedin.link_button("Linkedin", "https://www.linkedin.com/in/adrianmoldes/")
+        github.link_button("GitHub", "https://github.com/AdrianMoldes")
+
+    ################################################################
+    ########## Eduardo ##########
+
+    with columna_eduardo:
+        st.header(":orange[_Eduardo Velazco_]")
+        eduardo = cv2.imread(r"data/Eduardo.png")
+        eduardo = cv2.cvtColor(eduardo, cv2.COLOR_BGR2RGB)
+        eduardo = cv2.resize(eduardo, (width, height))
+        st.image(eduardo)
+
+        # Links:
+        linkedin, github = st.columns((1.2,1))
+
+        linkedin.link_button("Linkedin", "https://www.linkedin.com/in/eduardovelazco/")
+        github.link_button("GitHub", "https://github.com/e-velazco")
+
+    ################################################################
+    st.markdown(4 * '<br>', unsafe_allow_html=True)
+    ########## Esteban ##########
+
+    with columna_esteban:
+        st.header(":orange[&nbsp;&nbsp;&nbsp;_Esteban Pérez_]")
+
+        esteban = cv2.imread(r"data/Esteban.png")
+        esteban = cv2.cvtColor(esteban, cv2.COLOR_BGR2RGB)
+        esteban = cv2.resize(esteban, (width, height))
+        st.image(esteban)
+
+        # Links:
+        linkedin, github = st.columns((1.2,1))
+
+        linkedin.link_button("Linkedin", "https://www.linkedin.com/in/esteban-rafael-perez-lizardo/")
+        github.link_button("GitHub", "https://github.com/estebanrpl")
+
+    ################################################################
+    ########## Urko ##########
+
+    with columna_urko:
+        st.header(":orange[&nbsp;&nbsp;&nbsp;_Urko Regueiro_]")
+
+        urko = cv2.imread(r"data/Urko.png")
+        urko = cv2.cvtColor(urko, cv2.COLOR_BGR2RGB)
+        urko = cv2.resize(urko, (width, height))
+        st.image(urko)
+
+        # Links:
+        linkedin, github = st.columns((1.2,1))
+
+        linkedin.link_button("Linkedin", "https://www.linkedin.com/in/urkoregueiro/")
+        github.link_button("GitHub", "https://github.com/UrkoRegueiro")
+
+    st.markdown('<p class="patata">HOLA PATTATA</p>', unsafe_allow_html=True)
 
 
 
--- tech_job_app.py
+++ tech_job_app.py
@@ -35,17 +35,16 @@ def tech_app():
     ################ TITULO ########################
     st.markdown("<h1 style='text-align: center;color: orange; font-size: 3em;'>Un viaje por el mercado Tech español</h1>", unsafe_allow_html=True)
 
-    st.divider()
-    st.divider()
+    st.image("divider.png")
     ################ INTRO #######################
     col_1, col_2, col_3 = st.columns((0.2,1,0.2))
 
     col_2.markdown(
-        '<p class="big-font">Esta plataforma es el resultado del trabajo en equipo llevado por Adrián Moldes, Eduardo Velazco, Esteban Pérez y Urko Regueiro como proyecto final del bootcamp de <span style="font-weight: bold; color: orange">Data Science e Inteligencia Artificial</span> de la escuela <span style="font-weight: bold; color: orange">HACK A BOSS</span>.<br>'
+        '<p class="big-font">Esta plataforma es el resultado del trabajo en equipo llevado a cabo por Adrián Moldes, Eduardo Velazco, Esteban Pérez y Urko Regueiro como proyecto final del bootcamp de <span style="font-weight: bold; color: orange">Data Science e Inteligencia Artificial</span> de la escuela <span style="font-weight: bold; color: orange">HACK A BOSS</span>.<br>'
         'La idea de realizar un estudio pormenorizado del mercado laboral tecnólogico en España surge de nuestro propio interés en podernos incorporar a él, entendiendo al menos de forma general cuáles son las necesidades actuales y reales de las empresas que requieren este tipo de perfiles. Para conseguir nuestro objetivo, hemos llevado a cabo la tarea de extraer, transformar, cargar y analizar datos de los portales de empleo más utilizados con el objetivo de conseguir información valiosa y detallada, obteniendo una primera muestra de más de 18000 datos que seguiremos ampliando y actualizando.<br><br>'
         'En la sección <span style="font-weight: bold; color: orange">Una visión general</span> hemos querido proporcionar una panorámica completa de la situación del mercado tecnológico español. Desde la demanda de empleos por sector hasta las habilidades más demandadas. Reconocemos la importancia de comprender las tendencias generales antes de sumergirse en detalles más específicos.<br>'
         'En <span style="font-weight: bold; color: orange">Explora el mercado</span> hemos brindado la posibilidad de personalizar tu exploración, pudiendo descubrir qué stack tecnológico se adapta mejor a cada sector o entender la distribución salarial por comunidad autónoma a través de gráficos interactivos. Creemos que esta personalización permitirá obtener información precisa y relevante para saciar tu curiosidad.<br>'
-        'Por último hemos diseñado un <span style="font-weight: bold; color: orange">Predictor Salarial</span> montado sobre dos modelos de Machine Learning, que te permitirá, especificando ciertos parámetros, obtener un rango salarial estimado ajustado a tus características.<br><br>'
+        'Por último hemos diseñado un <span style="font-weight: bold; color: orange">Predictor Salarial</span> montado sobre dos modelos de Machine Learning que te permitirán, especificando ciertos parámetros, obtener un rango salarial estimado ajustado a tus características.<br><br>'
         'Queremos destacar que este proyecto no solo es una iniciativa informativa, sino también un testimonio de nuestro aprendizaje en el mundo de la ciencia de datos e inteligencia artificial. Este sitio web es el producto de un esfuerzo colaborativo de nuestro talentoso equipo, y queremos compartir contigo los frutos de nuestro trabajo. Si deseas explorar más a fondo el proceso ETL y el código detrás de esta plataforma, en la sección <span style="font-weight: bold; color: orange">Acerca de</span> encontrarás el link a nuestros perfiles de LinkedIn y GitHub, donde se reune todo el código.<br>'
         'Estamos emocionados de ofrecerte una herramienta que esperamos sea valiosa para tu comprensión del mercado laboral tecnológico en España. <br> <span style="font-weight: bold; font-size: 1.075em; color: orange">¡Sumérgete, explora y encuentra la información que te ayudará a avanzar en tu carrera en el apasionante mundo tech!</span></p>',
         unsafe_allow_html=True)
